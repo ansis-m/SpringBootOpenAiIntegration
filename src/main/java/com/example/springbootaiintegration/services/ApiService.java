9c168b5a07b05dd94f71372ce1c6c3ded5df99a1
ansis@home <ansis@home>
2024-02-03 20:02:27 +0200

    calling openAi api while preserving context

--- src/main/java/com/example/springbootaiintegration/services/ApiService.java
+++ src/main/java/com/example/springbootaiintegration/services/ApiService.java
@@ -1,6 +1,10 @@
 package com.example.springbootaiintegration.services;
 
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.ai.chat.messages.AssistantMessage;
+import org.springframework.ai.chat.messages.Message;
+import org.springframework.ai.chat.messages.MessageType;
+import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatClient;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -8,6 +12,9 @@ import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.Map;
 
 @Service
 public class ApiService {
@@ -22,16 +29,26 @@ public class ApiService {
                 .withDefaultOptions(OpenAiChatOptions.builder()
                                                      .withModel("gpt-3.5-turbo")
                                                      .withTemperature(0.4F)
-                                                     .withMaxTokens(200)
+                                                     .withMaxTokens(250)
                                                      .withN(1)
                                                      .build());
 
-        Flux<ChatResponse> response = chatClient.stream(
-                new Prompt("Which is the best programming language for enterprise web apps?"));
 
-        response.subscribe(r -> System.out.print(r.getResult().getOutput().getContent()));
+        Message m1 = new UserMessage("Which is the best programming language for enterprise web apps?");
+
+        ChatResponse response = chatClient.call(
+                new Prompt(m1));
+
+        System.out.print(response.getResult().getOutput().getContent());
+
+        AssistantMessage r1 = response.getResult().getOutput();
+
+        Message m2 = new UserMessage("Please elaborate 2.!");
+
+        response = chatClient.call(new Prompt(Arrays.asList(m1, r1, m2)));
 
 
+        System.out.print("\n\n\nSecond response:" + response.getResult().getOutput().getContent());
 
     }
 
