5fe3904dfa032a9d0369df5727bb3dbc3f06fdab
ansis@home <ansis@home>
2024-03-24 15:16:50 +0200

    code styling

--- frontend/src/components/Reply/Reply.tsx
+++ frontend/src/components/Reply/Reply.tsx
@@ -1,10 +1,17 @@
 import {ReplyProps} from "./props";
 import React from "react";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { solarizedlight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import "../Reply/Reply.css";
 
 
 const Reply: React.FC<ReplyProps> = ({messages}) => {
-    return (<div className="response"> {messages.join('')} </div>);
+    return (<div className="response"> {messages.join('').split("```").map((part, index) => {
+        if (index % 2 === 0 ) {
+            return (<div key={index}>{part}</div>);
+        }
+        return <SyntaxHighlighter key={index} style={solarizedlight} language={"java"}>{part}</SyntaxHighlighter>
+    })} </div>);
 };
 
 export default Reply;
